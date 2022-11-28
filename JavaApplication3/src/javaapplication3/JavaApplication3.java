cbac1273a8ea72f2822396c50f7f477ef84aae3b
Gurleen Singh <113394478+KharoudGarry@users.noreply.github.com>
2022-11-27 19:24:36 -0500

    Add files via upload

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -5,11 +5,9 @@
  */
 package javaapplication3;
 
-import java.util.Scanner;
-
 /**
  *
- * @author Devilsoul
+ * @author hp
  */
 public class JavaApplication3 {
 
@@ -18,14 +16,6 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a,b,c;
-        Scanner s =new Scanner(System.in);
-        System.out.print("Enter 1st number: ");
-        a =s.nextInt();
-        System.out.print("Enter 2nd number: ");
-        b =s.nextInt();
-        c=a+b;
-        System.out.println("sum: "+c);
     }
     
 }
